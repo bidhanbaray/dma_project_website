d9d3aa8ddbb41f9dc832c141c71d8ee338713b65
bidhanbaray <bidhanbaray03agmail.com>
2021-08-07 14:56:10 +0000

    latest commit 7th august 2021

--- projects/site1/home/views.py
+++ projects/site1/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from patient_view.models import patient_key
 
 
 def index(request):
-    return render(request, "home.html")
+    if patient_key.objects.filter(patient_id=request.user.username).count()>0:
+        is_patient = True
+        return render(request, "home.html",{'is_patient':is_patient})
+    else:
+        return render(request, "home.html")
